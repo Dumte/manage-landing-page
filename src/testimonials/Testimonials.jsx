a85ee8cf4d90b157d3951c783713f028cbb92b58
dumte <dummy4real@gmail.com>
2025-06-20 21:52:21 +0100

    Add comments for clarity in Testimonials component

--- src/testimonials/Testimonials.jsx
+++ src/testimonials/Testimonials.jsx
@@ -1,11 +1,13 @@
 const Testimonials = () => {
   return (
     <section id="testimonials">
+      {/* Testimonial Heading */}
       <div className="max-w-6xl px-5 mx-auto mt-32 text-center">
         <h2 className="text-4xl font-bold text-center">
           What's Different About Manage?
         </h2>
 
+        {/* Testimonial Cards */}
         <div className="flex flex-col mt-24 md:flex-row md:space-x-6 ">
           <div className="flex flex-col items-center p-6 space-y-6 rounded-lg bg-veryLightGray md:w-1/3">
             <img
@@ -50,6 +52,7 @@ const Testimonials = () => {
           </div>
         </div>
 
+        {/* Get Started Button */}
         <div className="my-16">
           <a
             href="#"
